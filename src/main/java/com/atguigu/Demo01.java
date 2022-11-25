5abcf172a3485982a497d53a46fb71c0bd0d3773
Gitee-Looper <zhangxiaofanvipc@163.com>
2022-11-25 23:29:19 +0800

    打印答案

--- src/main/java/com/atguigu/Demo01.java
+++ src/main/java/com/atguigu/Demo01.java
@@ -8,5 +8,7 @@ public class Demo01 {
     public static void main(String[] args) {
         int a = 0;
         int b = 10;
+        int c = a + b;
+        System.out.println("c = " + c);
     }
 }
